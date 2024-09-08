19d20a7893b9b713986c7dc7af97794286ab4150
fernandocbhorta <151946784+fernandocbhorta@users.noreply.github.com>
2024-09-08 17:49:21 -0600

    Assignment 2
    First attempt, let's hope it's correct

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Page() {
+    return (
+      <main>
+        <ul>
+            <li>Fernando C B Horta</li>
+            <li><Link href="https://github.com/fernandocbhorta/cprg306-assignments">Github.com CPRG-306 Assignments</Link></li>
+        </ul>
+      </main>
+    );
+  }
