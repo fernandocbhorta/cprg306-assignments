83157c0e4574cab7cf7881b5ced75c611fdd9bb7
fernandocbhorta <151946784+fernandocbhorta@users.noreply.github.com>
2024-11-08 07:36:43 -0700

    week10 beginning

---
+++ app/week-10/page.js
@@ -0,0 +1,27 @@
+"use client";
+
+import { useUserAuth } from "./_utils/auth-context";
+import Link from "next/link";
+
+export default function Page() {
+  const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+
+  console.log(user);
+
+  return (
+    <div>
+      <h1>Week 10</h1>
+      <p>{user ? "Hi there!" : "Please sign in to see the shopping list"}</p>          
+      
+      <p>
+        {user ? (<p><Link href="./week-10/shopping-list">See the shopping list</Link></p>) : (<p>&nbsp;</p>)}
+        {user ? (
+          
+          <button onClick={firebaseSignOut}>Sign Out</button>
+        ) : (
+          <button onClick={gitHubSignIn}>Sign In with GitHub</button>
+        )}
+      </p>
+    </div>
+  );
+}
