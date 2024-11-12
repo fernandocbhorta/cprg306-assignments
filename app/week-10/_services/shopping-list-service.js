a71b86211c6ab7adef5ab51c987101e591ad9474
fernandocbhorta <151946784+fernandocbhorta@users.noreply.github.com>
2024-11-12 08:06:53 -0700

    week10

---
+++ app/week-10/_services/shopping-list-service.js
@@ -0,0 +1,28 @@
+import { db } from "../_utils/firebase";
+import { collection, getDocs, addDoc, where, query } from "firebase/firestore";
+
+export async function getItems(userid) {
+    const q = query(
+        collection(db, "users", userid, "items"),
+        where("quantity", ">", 0)
+    );
+    const querySnapshot = await getDocs(q);
+    
+    const items = [];
+    querySnapshot.forEach((doc) => {
+        items.push({ id: doc.id, ...doc.data() });
+    });
+    return items; 
+    
+}
+
+export async function addItem(item, userid) {
+    console.log("Adding item:", item);
+    console.log("User ID:", userid);
+    try {
+        const docRef = await addDoc(collection(db, "users", userid, "items"), item);
+        console.log("Document written with ID: ", docRef.id);
+    } catch (error) {
+        console.error("Error adding document: ", error); 
+    }
+}
